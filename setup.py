71e59b60a30228944332cf91fb33dd21f14781f7
pipekorsi <porsika@gmail.com>
2024-10-18 16:17:16 +0200

    resolving dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='isomut2py',
-    version='2.0.1',
+    version='2.0.8',
     description='A mutation detection software from NGS data with multiple filtering and plotting features',
     url='http://github.com/pipekorsi/isomut2',
     author='Orsolya Pipek',
@@ -10,13 +10,14 @@ setup(
     license='MIT',
     packages=['isomut2py'],
     package_data={'isomut2py': ['alexandrovSignatures/*.csv', 'C/*']},
-    install_requires=['matplotlib>=2.0.0',
-                      'pandas>=0.20.3',
-                      'Theano>=0.9.0',
-                      'pymc3>=3.1',
-                      'setuptools>=33.1.1',
-                      'seaborn>=0.8',
-                      'scipy>=0.19.1',
-                      'numpy>=1.12.1',
-                      'biopython']
+    install_requires=['matplotlib==3.0.0',
+                      'pandas==0.20.3',
+                      'theano==1.0.5',
+                      'pymc3==3.2',
+                      'setuptools==33.1.1',
+                      'seaborn==0.8',
+                      'scipy==0.19.1',
+                      'numpy==1.12.1',
+                      'biopython',
+                      'joblib==0.11']
 )
